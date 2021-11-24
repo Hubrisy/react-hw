1fb3d27639c38deb2ca5af14009175e4b08365a6
Your Name <Hubrisy>
2021-11-24 14:36:02 +0200

    new changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+
+  const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+    const author = {
+      name: "Anakin skywalker",
+      photo: ANAKIN_IMAGE,
+      nickname: "@dart_vader",
+      content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+      image: RAY_IMAGE,
+      date: " · 26 февр."
+    }
+  return(
+    <div className = "container">
+    <img src = {ANAKIN_IMAGE} className = "avtor-img" alt = "" />
+    <div className = "text">{author.name} <img src = "https://w7.pngwing.com/pngs/267/59/png-transparent-blue-and-white-check-logo-illustration-verified-badge-logo-youtube-youtube-thumbnail.png" className = "lil" alt = ""/>  {author.nickname} {author.date}<br/>
+    {author.content}
     </div>
-  );
+    <img src = {RAY_IMAGE} className = "post-img" alt = "" />
+</div>
+  )
 }
 
 export default App;
